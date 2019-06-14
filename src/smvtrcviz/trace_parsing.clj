860c81faa368e3cd1c3006a70ccc4ce49aa750e8
felixlinker <linkerfelix@gmail.com>
2019-06-14 19:13:40 +0200

    Add trace parser

---
+++ src/smvtrcviz/trace_parsing.clj
@@ -0,0 +1,48 @@
+(ns smvtrcviz.trace-parsing
+  (:require [clojure.java.io :as io]
+            [clojure.xml :as xml]
+            [clojure.edn :as edn]))
+
+(defn ^:private select
+  [tag xml]
+  (filter #(= (:tag %) tag)
+          (or (:content xml) xml)))
+
+(defn ^:private read-vars
+  [node]
+  (let [values (select :value (:content node))]
+    (into {}
+          (map (fn [value-node]
+                 [(:variable (:attrs value-node))
+                  (first (:content value-node))])
+               values))))
+
+(defn ^:private parse-node
+  [node-xml]
+  (let [nodes (map (comp first #(select % node-xml))
+                   '(:state :combinatorial :input))
+        [state combinatorial input] nodes
+        id (first (filter (comp not nil?)
+                          (map (comp :id :attrs)
+                               nodes)))]
+    [id {:input (read-vars input)
+         :state (read-vars state)
+         :combinatorial (read-vars combinatorial)}]))
+
+(defn parse-trace
+  [xml]
+  (->> xml
+       (select :counter-example)
+       (first)
+       (select :node)
+       (map parse-node)
+       (into (sorted-map-by #(apply compare
+                                    (map edn/read-string %&))))))
+
+(defn load-trace
+  [path]
+  (-> path
+      (io/input-stream)
+      (xml/parse)
+      (xml-seq)
+      (parse-trace)))
