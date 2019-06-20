eca08b5efc710432f0afb330337130f97a2e2669
felixlinker <linkerfelix@gmail.com>
2019-06-20 21:06:09 +0200

    Fix license in project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject smvtrcviz "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license {:name "The MIT License (MIT)"
+            :url "https://mit-license.org/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [hiccup "1.0.5"]]
   :main ^:skip-aot smvtrcviz.core
