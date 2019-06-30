d4d1188f4206b0fbc860b0360d245832b038c286
felixlinker <linkerfelix@gmail.com>
2019-06-30 19:12:33 +0200

    Make trace-mapping more idiomatic

--- src/smvtrcviz/trace_mapping.clj
+++ src/smvtrcviz/trace_mapping.clj
@@ -1,14 +1,6 @@
 (ns smvtrcviz.trace-mapping
   (:require [hiccup.core :as hiccup]
-            [clojure.set :as set]
-            [clojure.edn :as edn])
-  (:use smvtrcviz.util))
-
-(defn- get-all-vars
-  "Get all variables in all steps of a trace of a given type as key, which can
-be one of :state, :input, :combinatorial."
-  [trace key]
-  (apply set/union (map (comp key second) trace)))
+            [clojure.edn :as edn]))
 
 (def ^:private bvr #"(-?)0(s|u)d(\d+)_(\d+)")
 
@@ -25,67 +17,85 @@ string."
                          (list bit-str)))
       (subs bit-str subsfrom))))
 
-(defn- try-parse
+(defn- pretty-print
   "Tries to parse a bit-vector as binary string; if this fails, value is
 returned unchanged."
   [value]
-  (let [match (re-matches bvr value)]
-    (if (nil? match)
-      value
-      (let [[_ sign type size-str num-val-str] match
-            signed (and (not (empty? sign)) (= type "s"))
-            size (edn/read-string size-str)
-            num-val (* (edn/read-string num-val-str) (if signed -1 1))]
-        (resize (Integer/toBinaryString num-val) size signed)))))
+  (if (nil? value)
+    "..."
+    (let [match (re-matches bvr value)]
+      (if (nil? match)
+        value
+        (let [[_ sign type size-str num-val-str] match
+              signed (and (not (empty? sign)) (= type "s"))
+              size (edn/read-string size-str)
+              num-val (* (edn/read-string num-val-str) (if signed -1 1))]
+          (resize (Integer/toBinaryString num-val) size signed))))))
 
-(defn- prettyv-vars
+(defn- remove-duplicate-vars
+  [var-history]
+  (loop [last-val nil
+         red []
+         iterated var-history]
+    (if (empty? iterated)
+      red
+      (let [val (as-> (first iterated) head
+                  (if (= head last-val) nil head))]
+        (recur (if (nil? val)
+                 last-val
+                 val)
+               (conj red val)
+               (rest iterated))))))
+
+(defn- pretty-print-vars
   "Gets the history of all values of a variable belonging to a group, e.g.
 :state, in a trace. Values will be parsed if possible and aslong as values don't
 change, they'll be replaced with \"...\"."
-  [trace group var-name]
-  (let [vals (mapv (comp #(get % var-name) group second)
-                   trace)]
-    (map #(if (nil? %) "..." (try-parse %))
-         (reduce #(let [last-val (peek-not-nil %1)]
-                    (conj %1 (if (= last-val %2)
-                               nil
-                               %2)))
-                 []
-                 vals))))
+  [var-history]
+  (->> var-history
+       (remove-duplicate-vars)
+       (map pretty-print)))
 
 (defn- var-to-row
   "Get a table row representing the values of a given variable belonging to a
 certain group, e.g. :state."
-  [trace group var-name]
-  (conj (map #(vector :td %) (prettyv-vars trace group var-name))
-        [:th var-name]))
+  [prepend-elem var-name var-history]
+  (cond->> var-history
+    true (pretty-print-vars)
+    true (map (partial vector :td))
+    true (interpose [:td])
+    (not (nil? prepend-elem)) (cons prepend-elem)
+    true (cons [:th var-name])
+    true (cons :tr)
+    true (into [])))
+
+(defn- collect-vars-for
+  [trace group]
+  (loop [step 1
+         var-traces {}]
+    (let [vars (group (trace step))
+          base (into [] (range (- step 1)))]
+      (if vars
+        (recur (inc step)
+               (reduce-kv #(assoc %1 %2 (conj (get %1 %2 base)
+                                              %3))
+                          var-traces
+                          vars))
+        var-traces))))
 
 (defn trace2table
   "Transform a trace into an html table reflecting how values changed over time
 from left to right."
   [trace]
-  (let [vars (reduce #(assoc %1 %2 (get-all-vars trace %2))
-                     {}
-                     '(:input :state :combinatorial))
-        dummy-row (mapv (constantly [:td])
-                        (range (count trace)))
-        rows (-> (update-map #(map (partial var-to-row trace %1) (keys %2))
-                             vars)
-                 (update :input
-                         (partial map (partial update-tail
-                                               #(interleave-strict dummy-row %))))
-                 (update :state
-                         (partial map (partial update-tail
-                                               #(interleave-strict %
-                                                                   dummy-row))))
-                 (update :combinatorial
-                         (partial map (partial update-tail
-                                               #(interleave-strict % dummy-row)))))
-        print-rows (fn [group]
-                     (map (comp pop (partial into []) #(cons :tr %))
-                          (sort-by (comp second first)
-                                   (group rows))))]
+  (let [ks (list :input :state :combinatorial)
+        vars (zipmap ks
+                     (map (partial collect-vars-for trace)
+                          ks))
+        print-rows #(->> %1
+                         (vars)
+                         (map (partial apply
+                                       (partial var-to-row %2))))]
     (hiccup/html [:table
-                  (print-rows :input)
-                  (print-rows :state)
-                  (print-rows :combinatorial)])))
+                  (print-rows :input [:td])
+                  (print-rows :state nil)
+                  (print-rows :combinatorial nil)])))
